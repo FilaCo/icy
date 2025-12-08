71ebc7da8757b13e5f4eeddf9a030c0df3f09888
Dmitrii Chizha <chizha.dmitrii@huawei.com>
2025-12-08 18:54:58 +0300

    docs: add some comments

--- shell/icy/src/main.rs
+++ shell/icy/src/main.rs
@@ -4,6 +4,7 @@ use icy::{prelude::*, tracing};
 
 #[tokio::main]
 async fn main() {
+    // getting guard is necessary, because it needs to be alive during the whole `main` execution
     let _guard = tracing::init();
     info!("icy starting...");
     let mut cli = Cli::parse();
