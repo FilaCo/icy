b4bc71f22e85a4512d51841f8882977ebddfe15d
Dmitrii Chizha <chizha.dmitrii@huawei.com>
2025-12-10 14:57:06 +0300

    feat: detached mode arg

--- shell/icy/src/cli/handle.rs
+++ shell/icy/src/cli/handle.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
 use daemonize::Daemonize;
-use tracing::{info, trace};
+use tracing::info;
 
 use crate::{
     cli::{Cli, Commands},
@@ -13,15 +13,14 @@ impl Cli {
     pub async fn handle(&mut self) {
         let config_path = self.config.take().unwrap_or(config().to_path_buf());
         match self.command.take().unwrap_or_default() {
-            Commands::Open { attach } => self.handle_open(config_path, attach),
-            Commands::Close => self.handle_close(),
+            Commands::Shell { attach } => self.handle_shell(config_path, attach),
             Commands::Lock => self.handle_lock(),
         }
     }
 
-    fn handle_open(&mut self, config_path: PathBuf, attach: bool) {
+    fn handle_shell(&mut self, config_path: PathBuf, attach: bool) {
         if !attach {
-            trace!("detach icy from the current terminal");
+            info!("detach icy from the current terminal");
             Daemonize::new()
                 .start()
                 .expect("unable to detach icy from the current terminal");
@@ -31,8 +30,6 @@ impl Cli {
             .run_with(|| Shell::new(config_path))
             .expect("unable to run iced::daemon")
     }
-
-    fn handle_close(&mut self) {}
 
     fn handle_lock(&mut self) {}
 }
--- shell/icy/src/cli/parser.rs
+++ shell/icy/src/cli/parser.rs
@@ -5,29 +5,22 @@ use clap::{Parser, Subcommand};
 use crate::cli::style::CLI_STYLING;
 
 #[derive(Parser, Default, Debug)]
-#[command(version, about, styles=CLI_STYLING)]
+#[command(version, about, long_about, styles=CLI_STYLING)]
 #[command(propagate_version = true)]
 pub struct Cli {
     /// Sets a custom config file
     #[arg(short, long, value_name = "FILE")]
-    pub(crate) config: Option<PathBuf>,
+    pub config: Option<PathBuf>,
     #[command(subcommand)]
-    pub(crate) command: Option<Commands>,
+    pub command: Option<Commands>,
 }
 
 #[derive(Subcommand, Debug)]
 pub enum Commands {
-    Open {
-        /// Attached mode: Run icy in the current terminal
+    Shell {
+        /// Detach icy shell from the current terminal
         #[arg(short, long)]
-        attach: bool,
+        detached: bool,
     },
-    Close,
     Lock,
-}
-
-impl Default for Commands {
-    fn default() -> Self {
-        Self::Open { attach: false }
-    }
 }
